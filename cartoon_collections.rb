9c3748ed78f9179375d7fb261e3e0ddea8f01660
Adham Muhammad <admuaq@gmail.com>
2018-07-10 13:29:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,10 @@ def roll_call_dwarves(order)# code an argument here
   
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(call)# code an argument here
   # Your code here
+  call.collect do |element|
+    
 end
 
 def long_planeteer_calls# code an argument here
