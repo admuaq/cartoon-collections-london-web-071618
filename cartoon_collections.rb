37376d010e82e08022ada766db3227189aaf9ad7
Adham Muhammad <admuaq@gmail.com>
2018-07-10 13:26:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(order)# code an argument here
   # Your code here
   order.each_with_index do |element, index|
-    "#{index}. #{element}"
+   puts "#{index}. #{element}"
   end 
   
 end
